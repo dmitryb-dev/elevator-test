ec58f1e94d7d1266d3f502cccee1e00e93518423
Dmitry Barannik <dmitryb.dev@gmail.com>
2021-11-07 11:22:37 +0200

    Added java main

--- src/main/kotlin/elevator/Main.kt
+++ src/main/kotlin/elevator/Main.kt
@@ -1,5 +1,10 @@
 package elevator
 
+import mu.KotlinLogging
+
 fun main() {
     val creatingOfObjectDoesNotRequireNewKeyword = KotlinIsSimple("")
+    log.info { "App started" }
 }
+
+private val log = KotlinLogging.logger {}
